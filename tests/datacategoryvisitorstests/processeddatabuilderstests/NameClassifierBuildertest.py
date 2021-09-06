3dfec12b6937d70c8628db19d1e76924efd538e6
SPD3 <sean@ayerdoyles.org>
2021-09-06 00:04:49 -0400

    Test code for the builder that classifies names based on their titles

--- tests/datacategoryvisitorstests/processeddatabuilderstests/NameClassifierBuildertest.py
+++ tests/datacategoryvisitorstests/processeddatabuilderstests/NameClassifierBuildertest.py
@@ -3,10 +3,54 @@ import unittest
 
 class NameClassifierBuilderTest (unittest.TestCase):
     """Tests the NameClassifierBuilder"""
+    def setUp(self) -> None:
+        self.nameClassifierBuilder = NameClassifierBuilder()
 
     def test_getTitle(self):
         """Makes sure that getTitle does find titles"""
-        name = "Mrs. ldajfhgp"
-        nameClassifierBuilder = NameClassifierBuilder()
-        title = nameClassifierBuilder.getTitle(name)
-        self.assertEquals(title, "Mrs")
+        def checkNameAndTitle(name, titlesolution):
+            title = self.nameClassifierBuilder.getTitle(name)
+            self.assertEquals(titlesolution, title)
+
+        checkNameAndTitle("Mrs. ldajfhgp", "Mrs")
+        checkNameAndTitle("dlsfajkMrdlkjaf", "Mr")
+        checkNameAndTitle("dagddgwdasJonkheer", "Jonkheer")
+
+    def testInitializeNameMapping(self) -> None:
+        """Makes sure that initializeNameMapping() creates a list 6 bins long 
+        and is all 0s"""
+        self.nameClassifierBuilder.initializeNameMapping()
+        self.assertEquals(type(self.nameClassifierBuilder.currentNameMapping), list)
+        solution = [0.0,0.0,0.0,0.0,0.0,0.0]
+        self.assertEquals(solution, self.nameClassifierBuilder.currentNameMapping)
+
+    def testMapTitle(self) -> None:
+        """Makes sure that mapTitle() maps various titles to the correct bin"""
+        def testNewTitle(name, solution):
+            self.nameClassifierBuilder.initializeNameMapping()
+            title = self.nameClassifierBuilder.getTitle(name)
+            self.nameClassifierBuilder.mapTitle(title)
+            self.assertEquals(solution, self.nameClassifierBuilder.currentNameMapping)
+
+        solution = [1.0,0.0,0.0,0.0,0.0,0.0]
+        testNewTitle("jslghaldfaCollgja lgn awfggad", solution)
+        
+        solution = [0.0,0.0,1.0,0.0,0.0,0.0]
+        testNewTitle("fsdj Mrs. afjdlgaj", solution)
+
+        solution = [0.0,0.0,0.0,0.0,0.0,1.0]
+        testNewTitle("jslghaldfagja lgn awfggad", solution)
+
+    def testBuildProcessedData(self) -> None:
+        """Makes sure that for every name passed in it's title is binned and 
+        appended to processedData"""
+        preprocessedData = ["jglskajgCaptgakljd", "MissMissMissMiss", "Don akgjdflagjkl", "aafdfdDr"]
+        self.nameClassifierBuilder.preprocessedData = preprocessedData
+        self.nameClassifierBuilder.buildProcessedData()
+        solution = [
+            [1.0,0.0,0.0,0.0,0.0,0.0],
+            [0.0,1.0,0.0,0.0,0.0,0.0],
+            [1.0,0.0,0.0,0.0,0.0,0.0],
+            [0.0,0.0,0.0,1.0,0.0,0.0]
+        ]
+        self.assertEquals(solution, self.nameClassifierBuilder.processedData)
