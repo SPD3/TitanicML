329c7fff4d0291f253f32fd3f0c34b3a903ab24f
SPD3 <sean@ayerdoyles.org>
2021-09-06 00:29:03 -0400

    Test code for the builder that just scales its input by a specified factor.

---
+++ tests/datacategoryvisitorstests/processeddatabuilderstests/ScaleDataBuildetest.py
@@ -0,0 +1,20 @@
+import unittest
+
+import numpy as np
+from datacategoryvisitors.processeddatabuilders.ScaleDataBuilder import ScaleDataBuilder
+
+class ScaleDataBuilderTest (unittest.TestCase):
+    """Tests the ScaleDataBuilder class"""
+    def setUp(self) -> None:
+        """Sets up then scaleDataBuilder with a scale factor of 0.5"""
+        self.scaleDataBuilder = ScaleDataBuilder(0.5)
+
+    def testBuildProcessedData(self) -> None:
+        """Makes sure that the preprocessed data is scaled by 0.5 and then is 
+        appended onto the end of processedData"""
+
+        preprocessedData = [1.0, 82, 53, 1024]
+        self.scaleDataBuilder.preprocessedData = preprocessedData
+        self.scaleDataBuilder.buildProcessedData()
+        solution = [[0.5], [41], [26.5], [512]]
+        self.assertEquals(solution, self.scaleDataBuilder.processedData)
