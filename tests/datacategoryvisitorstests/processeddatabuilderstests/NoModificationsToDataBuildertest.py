958ea944e21cec8bf2f8a92cbf7b973db5c66ccb
SPD3 <sean@ayerdoyles.org>
2021-09-06 00:09:34 -0400

    Test code for the processed data builder that does not change the content of the preprocessed data

---
+++ tests/datacategoryvisitorstests/processeddatabuilderstests/NoModificationsToDataBuildertest.py
@@ -0,0 +1,22 @@
+import unittest
+
+from datacategoryvisitors.processeddatabuilders.NoModificationsToDataBuilder import NoModificationsToDataBuilder
+
+class NoModificationsToDataBuilderTest (unittest.TestCase):
+    """Tests the NoModificationsToDataBuilder class"""
+    def setUp(self) -> None:
+        self.noModificationsToDataBuilder = NoModificationsToDataBuilder()
+
+    def testBuildProcessedData(self) -> None:
+        """Makes sure that the content of preprecessedData stays the same and 
+        that it's format is only changed in processedData to be a list of lists"""
+        preprocessedData = [1.0, "Goodbye", 1234, [1,2,3,4]]
+        self.noModificationsToDataBuilder.preprocessedData = preprocessedData
+        self.noModificationsToDataBuilder.buildProcessedData()
+        solution = [
+            [1.0],
+            ["Goodbye"],
+            [1234],
+            [[1,2,3,4]]
+        ]
+        self.assertEquals(solution, self.noModificationsToDataBuilder.processedData)
