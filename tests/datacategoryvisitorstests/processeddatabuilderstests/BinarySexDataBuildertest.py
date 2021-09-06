f58b6cdfb8e9786e29c4c6f623f93b1f93d9303a
SPD3 <sean@ayerdoyles.org>
2021-09-05 23:22:08 -0400

    Created test code for the BinarySexDataBuilder

--- tests/datacategoryvisitorstests/processeddatabuilderstests/BinarySexDataBuildertest.py
+++ tests/datacategoryvisitorstests/processeddatabuilderstests/BinarySexDataBuildertest.py
@@ -5,4 +5,12 @@ class BinarySexDataBuilderTest (unittest.TestCase):
     """Tests the BinarySexDataBuilder class"""  
     def setUp(self) -> None:
         self.ageClassifierBuilder = BinarySexDataBuilder()
-        
+
+    def testBuildProcessedData(self):
+        """Makes sure that buildProcessedData correctly categorizes males and 
+        females in processedData"""
+        preprocessedData = ["male","male","female","male","female"]
+        self.ageClassifierBuilder.preprocessedData = preprocessedData
+        self.ageClassifierBuilder.buildProcessedData()
+        solution = [[1.0],[1.0],[0.0],[1.0],[0.0]]
+        self.assertEquals(solution, self.ageClassifierBuilder.processedData)
